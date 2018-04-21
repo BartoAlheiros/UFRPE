89b78b0a45ec71a3eff1ad5657202674e0684540
Bartô Alheiros <alheirosb@gmail.com>
2018-04-20 21:18:49 -0300

    grafos att

---
+++ Algoritmos Em Grafos/Grafo/src/Grafo.java
@@ -0,0 +1,12 @@
+
+public class Grafo {
+	Float[] Vertices;
+	Float[] Arestas;
+	
+	public Grafo(Float[] vertices, Float[] arestas) {
+		super();
+		Vertices = vertices;
+		Arestas = arestas;
+	}
+		 
+}
